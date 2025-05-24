248d6a14ce790e67c4b31a95413658c1bbd4a4e2
jost0206 <jstevenrico@ucundinamarca.edu.co>
2025-05-23 22:54:59 -0500

    Delete web_contacto directory
    1

--- web_contacto/contact.js
+++
@@ -1,5 +0,0 @@
-function enviar() {
-    let email = document.getElementById('email').value;
-    alert("Gracias por contactarnos " + email);
-    document.getElementById('msg').innerHTML = "<script>alert('XSS en mensaje')</script>";
-}
